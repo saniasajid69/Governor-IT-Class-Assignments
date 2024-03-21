3c5d6a3ffb24154a214ffbf9a1b56588114a00cf
saniasajid69 <saniasajid69@gmail.com>
2024-03-21 05:43:28 +0000

    Add Simple Calculator

---
+++ Simple Calculator/index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+
+// Asking questions from users through inguirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number",type: "number",name:"firstNumber"},
+    {message: "Enter Second Number",type: "number",name:"secondNumber"},
+    {
+        message: "Select one Operator to perform Operation",
+        type: "list",
+        name:"operator",
+        choices: ["Addition","Subtraction","Multiplication","Division"],
+    },
+]);
+
+//Conditional statements If-Else
+
+if(answers.operator === "Addition"){
+      console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtraction"){
+      console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+
+    else if (answers.operator === "Division"){
+        console.log(answers.firstNumber / answers.secondNumber)
+    }
+    else{
+        console.log("Invalid Input")
+    }
